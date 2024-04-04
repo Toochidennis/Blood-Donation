45672825d84d69d089d28de0eee6c1bf0f421ec8
Toochidennis <dennistoochukwu@gmail.com>
2024-04-05 00:10:44 +0100

    New commit

--- app/src/main/java/com/devtoochi/blood_donation/fragments/SignUpMethodFragment.kt
+++ app/src/main/java/com/devtoochi/blood_donation/fragments/SignUpMethodFragment.kt
@@ -5,33 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.devtoochi.blood_donation.R
+import com.devtoochi.blood_donation.databinding.FragmentSignUpMethodBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 
 class SignUpMethodFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentSignUpMethodBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up_method, container, false)
+        binding = FragmentSignUpMethodBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.navigateBackButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        binding.hospitalButton.setOnClickListener {
+            findNavController().navigate(R.id.action_signUpMethodFragment_to_hospitalSignUpFragment)
+        }
+
+        binding.donorButton.setOnClickListener {
+            findNavController().navigate(R.id.action_signUpMethodFragment_to_donorSignUpFragment)
+        }
     }
 
 
