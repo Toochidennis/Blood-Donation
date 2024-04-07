736ca4a592f1d882b1e73b6dff1256989b5233b5
Toochidennis <dennistoochukwu@gmail.com>
2024-04-07 12:38:29 +0100

    New commit

---
+++ app/src/main/java/com/devtoochi/blood_donation/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.devtoochi.blood_donation
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.devtoochi.blood_donation.activities.LoginActivity
+import com.devtoochi.blood_donation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
+    }
+
+}
