40d69a98637c0a58c6ad7f81984e83e894ada2fa
Toochidennis <dennistoochukwu@gmail.com>
2024-04-08 01:15:42 +0100

    New commit

--- app/src/main/java/com/devtoochi/blood_donation/fragments/BloodBanksFragment.kt
+++ app/src/main/java/com/devtoochi/blood_donation/fragments/BloodBanksFragment.kt
@@ -1,4 +1,4 @@
-package com.devtoochi.blood_donation.dialogs
+package com.devtoochi.blood_donation.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -12,12 +12,8 @@ import com.devtoochi.blood_donation.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PostBloodRequestDialogFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class PostBloodRequestDialogFragment : Fragment() {
+
+class BloodBanksDialogFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,22 +31,14 @@ class PostBloodRequestDialogFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post_blood_request_dialog, container, false)
+        return inflater.inflate(R.layout.fragment_blood_banks, container, false)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PostBloodRequestDialogFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            PostBloodRequestDialogFragment().apply {
+            BloodBanksDialogFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
