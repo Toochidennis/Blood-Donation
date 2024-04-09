73b895d4f9c144f8f07fd1c304d35ee6b97068bb
Toochidennis <dennistoochukwu@gmail.com>
2024-04-09 16:55:50 +0100

    New commit

--- app/src/main/java/com/devtoochi/blood_donation/dialogs/DonorProfileEditProfileDialogFragment.kt
+++ app/src/main/java/com/devtoochi/blood_donation/dialogs/DonorProfileEditProfileDialogFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import com.devtoochi.blood_donation.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -14,10 +15,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [DonorProfileSettingsDialogFragment.newInstance] factory method to
+ * Use the [DonorProfileEditProfileDialogFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class DonorProfileSettingsDialogFragment : Fragment() {
+class DonorProfileEditProfileDialogFragment : DialogFragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,22 +36,14 @@ class DonorProfileSettingsDialogFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donor_profile_settings_dialog, container, false)
+        return inflater.inflate(R.layout.fragment_donor_edit_profile_dialog, container, false)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DonorProfileSettingsDialogFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            DonorProfileSettingsDialogFragment().apply {
+            DonorProfileEditProfileDialogFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
