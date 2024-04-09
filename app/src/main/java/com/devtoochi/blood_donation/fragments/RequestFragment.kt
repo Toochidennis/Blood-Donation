d4de456fdb1f97cb72d35da4c0b5b093c06d30f0
Toochidennis <dennistoochukwu@gmail.com>
2024-04-09 17:06:49 +0100

    New commit

--- app/src/main/java/com/devtoochi/blood_donation/fragments/RequestFragment.kt
+++ app/src/main/java/com/devtoochi/blood_donation/fragments/RequestFragment.kt
@@ -1,16 +1,19 @@
 package com.devtoochi.blood_donation.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.devtoochi.blood_donation.R
+import com.devtoochi.blood_donation.activities.BloodBankRequestActivity
 import com.devtoochi.blood_donation.databinding.FragmentRequestBinding
 
 
 class RequestFragment : Fragment() {
 
-    private lateinit var binding:FragmentRequestBinding
+    private lateinit var binding: FragmentRequestBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,13 +21,21 @@ class RequestFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentRequestBinding.inflate(inflater, container, false)
-       return binding.root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.postBloodRequestButton.setOnClickListener {
 
+        binding.postBloodRequestButton.setOnClickListener {
+            PostBloodRequestDialogFragment().show(
+                parentFragmentManager,
+                getString(R.string.post_blood_request)
+            )
+        }
+
+        binding.bloodBankButton.setOnClickListener {
+            startActivity(Intent(requireContext(), BloodBankRequestActivity::class.java))
         }
     }
 
