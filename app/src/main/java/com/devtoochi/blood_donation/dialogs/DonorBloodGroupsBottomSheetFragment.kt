893773d0d3a5934bd77211b9f976dbe8fe767984
Toochidennis <dennistoochukwu@gmail.com>
2024-04-09 16:55:40 +0100

    New commit

--- app/src/main/java/com/devtoochi/blood_donation/dialogs/DonorBloodGroupsBottomSheetFragment.kt
+++ app/src/main/java/com/devtoochi/blood_donation/dialogs/DonorBloodGroupsBottomSheetFragment.kt
@@ -4,23 +4,67 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import com.devtoochi.blood_donation.BR
 import com.devtoochi.blood_donation.R
+import com.devtoochi.blood_donation.adapters.GenericAdapter
+import com.devtoochi.blood_donation.backend.models.BloodGroup
+import com.devtoochi.blood_donation.databinding.FragmentDonorBloodGroupsBottomSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DonorBloodGroupsBottomSheetFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class DonorBloodGroupsBottomSheetFragment : Fragment() {
+
+class DonorBloodGroupsBottomSheetFragment(
+    private val onSelected: (String) -> Unit
+) : BottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentDonorBloodGroupsBottomSheetBinding
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donor_blood_groups_bottom_sheet, container, false)
+        binding = FragmentDonorBloodGroupsBottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        prepareData()
+
+    }
+
+    private fun prepareData() {
+        val bloodGroups = resources.getStringArray(R.array.blood_groups).toMutableList()
+        val newBloodGroups = mutableListOf<BloodGroup>()
+
+        bloodGroups.forEachIndexed { index, bloodGroup ->
+            newBloodGroups.add(BloodGroup("${index + 1}", bloodGroup))
+        }
+
+        setUpAdapter(bloodGroups = newBloodGroups)
+
+    }
+
+    private fun setUpAdapter(bloodGroups: MutableList<BloodGroup>) {
+        val bloodGroupAdapter = GenericAdapter(
+            itemList = bloodGroups,
+            itemResLayout = R.layout.item_fragment_donor_blood_groups_bottom_sheet,
+            bindItem = { binding, model ->
+                binding.setVariable(BR.blood, model)
+                binding.executePendingBindings()
+            }
+        ) { position ->
+            onSelected.invoke(bloodGroups[position].name)
+            dismiss()
+        }
+
+        binding.bloodGroupRecyclerview.apply {
+            hasFixedSize()
+            adapter = bloodGroupAdapter
+        }
     }
 
 
