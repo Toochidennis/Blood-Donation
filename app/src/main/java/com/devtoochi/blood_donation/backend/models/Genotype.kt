c71b2c6c9d3d39a8cde5d5a72e0d798baf92dc4d
Toochidennis <dennistoochukwu@gmail.com>
2024-04-09 16:56:35 +0100

    New commit

---
+++ app/src/main/java/com/devtoochi/blood_donation/backend/models/Genotype.kt
@@ -0,0 +1,3 @@
+package com.devtoochi.blood_donation.backend.models
+
+data class Genotype(val name: String)
--- app/src/main/java/com/devtoochi/blood_donation/dialogs/GenderBottomSheetFragment.kt
+++ app/src/main/java/com/devtoochi/blood_donation/dialogs/GenderBottomSheetFragment.kt
@@ -1,60 +1,43 @@
 package com.devtoochi.blood_donation.dialogs
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.devtoochi.blood_donation.R
+import com.devtoochi.blood_donation.databinding.FragmentGenderBottomSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.textview.MaterialTextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class GenderBottomSheetFragment(
+    private val onSelected: (String) -> Unit
+) : BottomSheetDialogFragment() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [GenderBottomSheetFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class GenderBottomSheetFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentGenderBottomSheetBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gender_bottom_sheet, container, false)
+        binding = FragmentGenderBottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GenderBottomSheetFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GenderBottomSheetFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.textView1.setOnClickListenerWithText()
+        binding.textView2.setOnClickListenerWithText()
+
     }
+
+    private fun View.setOnClickListenerWithText() {
+        if (this is MaterialTextView) {
+            this.setOnClickListener {
+                onSelected.invoke(this.text.toString())
+                dismiss()
+            }
+        }
+    }
+
 }
