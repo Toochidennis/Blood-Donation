9be0a1cc84864a1a5dd918cca9dd599173291274
Toochidennis <dennistoochukwu@gmail.com>
2024-04-09 16:55:30 +0100

    New commit

--- app/src/main/java/com/devtoochi/blood_donation/dialogs/ContactInfoDialogFragment.kt
+++ app/src/main/java/com/devtoochi/blood_donation/dialogs/ContactInfoDialogFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import com.devtoochi.blood_donation.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -13,13 +14,15 @@ private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 
-class ContactInfoDialogFragment : Fragment() {
+class ContactInfoDialogFragment : DialogFragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.FullScreenDialog)
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -35,15 +38,7 @@ class ContactInfoDialogFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ContactInfoDialogFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             ContactInfoDialogFragment().apply {
---
+++ app/src/main/java/com/devtoochi/blood_donation/dialogs/DatePickerDialog.kt
@@ -0,0 +1,45 @@
+package com.devtoochi.blood_donation.dialogs
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.Gravity
+import android.view.Window
+import com.devtoochi.blood_donation.R
+import com.devtoochi.blood_donation.databinding.DialogDatePickerBinding
+
+
+class DatePickerDialog(
+    context: Context,
+    private val onDateSelected: (String) -> Unit
+) : Dialog(context) {
+
+    private val binding = DialogDatePickerBinding.inflate(layoutInflater)
+
+    init {
+        window?.apply {
+            requestWindowFeature(Window.FEATURE_NO_TITLE)
+            setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            attributes?.windowAnimations = R.style.DialogAnimation
+            setGravity(Gravity.BOTTOM)
+        }
+        show()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.datePicker.let { datePicker ->
+            binding.saveButton.setOnClickListener {
+                val selectedDate =
+                    "${datePicker.year}-${datePicker.month + 1}-${datePicker.dayOfMonth}"
+
+                onDateSelected.invoke(selectedDate)
+                dismiss()
+            }
+        }
+    }
+}
