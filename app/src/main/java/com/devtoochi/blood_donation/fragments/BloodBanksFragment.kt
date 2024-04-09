f8ac2d5d005a22cf97d3f523d14859b2470916c0
Toochidennis <dennistoochukwu@gmail.com>
2024-04-09 16:55:07 +0100

    New commit

--- app/src/main/java/com/devtoochi/blood_donation/fragments/BloodBanksFragment.kt
+++ app/src/main/java/com/devtoochi/blood_donation/fragments/BloodBanksFragment.kt
@@ -7,8 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.devtoochi.blood_donation.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
@@ -32,6 +31,12 @@ class BloodBanksDialogFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_blood_banks, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // val usernameTuple = mutableListOf<String>()
     }
 
     companion object {
