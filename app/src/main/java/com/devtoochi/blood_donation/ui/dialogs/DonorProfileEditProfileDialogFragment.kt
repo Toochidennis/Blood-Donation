d2ed2e58f58776c1f3f05b6184a3038b76fb46ea
Toochidennis <dennistoochukwu@gmail.com>
2024-04-12 00:06:09 +0100

    update

--- app/src/main/java/com/devtoochi/blood_donation/ui/dialogs/DonorProfileEditProfileDialogFragment.kt
+++ app/src/main/java/com/devtoochi/blood_donation/ui/dialogs/DonorProfileEditProfileDialogFragment.kt
@@ -1,10 +1,11 @@
-package com.devtoochi.blood_donation.fragments
+package com.devtoochi.blood_donation.ui.dialogs
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import com.devtoochi.blood_donation.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -14,10 +15,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [DonorNotificationsFragment.newInstance] factory method to
+ * Use the [DonorProfileEditProfileDialogFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class DonorNotificationsFragment : Fragment() {
+class DonorProfileEditProfileDialogFragment : DialogFragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,22 +36,14 @@ class DonorNotificationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donor_notifications, container, false)
+        return inflater.inflate(R.layout.fragment_donor_edit_profile_dialog, container, false)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DonorNotificationsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            DonorNotificationsFragment().apply {
+            DonorProfileEditProfileDialogFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/devtoochi/blood_donation/ui/fragments/DonorNotificationsFragment.kt
+++ app/src/main/java/com/devtoochi/blood_donation/ui/fragments/DonorNotificationsFragment.kt
@@ -1,4 +1,4 @@
-package com.devtoochi.blood_donation.fragments
+package com.devtoochi.blood_donation.ui.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [DonorHomeFragment.newInstance] factory method to
+ * Use the [DonorNotificationsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class DonorHomeFragment : Fragment() {
+class DonorNotificationsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class DonorHomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donor_home, container, false)
+        return inflater.inflate(R.layout.fragment_donor_notifications, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class DonorHomeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment DonorHomeFragment.
+         * @return A new instance of fragment DonorNotificationsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            DonorHomeFragment().apply {
+            DonorNotificationsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
