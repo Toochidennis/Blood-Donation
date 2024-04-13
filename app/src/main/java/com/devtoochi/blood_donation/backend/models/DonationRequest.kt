5a6e0aa1019b7dbfc44f49d2b3a081e428ea0197
Toochidennis <dennistoochukwu@gmail.com>
2024-04-13 17:20:59 +0100

    New commit

---
+++ app/src/main/java/com/devtoochi/blood_donation/backend/models/DonationRequest.kt
@@ -0,0 +1,16 @@
+package com.devtoochi.blood_donation.backend.models
+
+data class DonationRequest(
+    var requestId: String = "",
+    var userId: String = "",
+    var donorId: String = "",
+    var name: String = "",
+    var address: String = "",
+    var state: String = "",
+    var imageUrl: String = "",
+    var bloodGroup: String = "",
+    var unit: String = "",
+    var note: String = "",
+    var requestDate: String = "",
+    var datePosted: String = ""
+)
