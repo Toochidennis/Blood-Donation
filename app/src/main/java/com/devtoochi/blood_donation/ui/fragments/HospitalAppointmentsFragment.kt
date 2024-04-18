d41a1935d41b84d714a36566b984119ac49ee1bb
Toochidennis <dennistoochukwu@gmail.com>
2024-04-18 18:54:10 +0100

    Update

--- app/src/main/java/com/devtoochi/blood_donation/ui/fragments/HospitalAppointmentsFragment.kt
+++ app/src/main/java/com/devtoochi/blood_donation/ui/fragments/HospitalAppointmentsFragment.kt
@@ -6,17 +6,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.devtoochi.blood_donation.R
+import com.devtoochi.blood_donation.databinding.FragmentHospitalAppointmentsBinding
+import com.devtoochi.blood_donation.ui.adapters.FragmentAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class HospitalAppointmentsFragment : Fragment() {
 
+    private lateinit var binding: FragmentHospitalAppointmentsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hospital_appointments, container, false)
+        binding = FragmentHospitalAppointmentsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpViewPager()
+    }
+
+    private fun setUpViewPager() {
+        val fragmentTitles = listOf("Scheduled", "Donated")
+
+        val fragmentAdapter = FragmentAdapter(requireActivity()).apply {
+            addFragment(HospitalAppointmentHistoryFragment())
+            addFragment(HospitalDonationHistoryFragment())
+        }
+
+        binding.viewPager.adapter = fragmentAdapter
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = fragmentTitles[position]
+        }.attach()
     }
 
 }
