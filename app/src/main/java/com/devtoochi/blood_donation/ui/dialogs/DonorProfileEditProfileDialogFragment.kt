01ad90d1a9f5e33c27c5c5d902f79656a7c70d8c
Toochidennis <dennistoochukwu@gmail.com>
2024-04-18 18:52:17 +0100

    Update

--- app/src/main/java/com/devtoochi/blood_donation/ui/dialogs/DonorProfileEditProfileDialogFragment.kt
+++ app/src/main/java/com/devtoochi/blood_donation/ui/dialogs/DonorProfileEditProfileDialogFragment.kt
@@ -1,53 +1,242 @@
 package com.devtoochi.blood_donation.ui.dialogs
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.devtoochi.blood_donation.R
+import com.devtoochi.blood_donation.backend.firebase.PersonDetailsManager.updatePersonalDetails
+import com.devtoochi.blood_donation.backend.models.Donor
+import com.devtoochi.blood_donation.backend.utils.Constants
+import com.devtoochi.blood_donation.databinding.FragmentDonorEditProfileDialogBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DonorProfileEditProfileDialogFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DonorProfileEditProfileDialogFragment : DialogFragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentDonorEditProfileDialogBinding
+    private lateinit var loadingDialog: LoadingDialog
+    private lateinit var sharedPreferences: SharedPreferences
+    private var profileId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        setStyle(STYLE_NORMAL, R.style.FullScreenDialog)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donor_edit_profile_dialog, container, false)
+        binding = FragmentDonorEditProfileDialogBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loadingDialog = LoadingDialog(requireContext())
+        sharedPreferences = requireActivity().getSharedPreferences(
+            Constants.PREF_NAME,
+            Context.MODE_PRIVATE
+        )
 
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DonorProfileEditProfileDialogFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        initData()
+        handleViewsClick()
+    }
+
+    private fun initData() {
+        with(sharedPreferences) {
+            profileId = getString("id", "")
+            binding.firstNameTextInput.setText(getString("firstname", ""))
+            binding.lastNameTextInput.setText(getString("lastname", ""))
+            binding.dateOfBirthTextInput.setText(getString("date_of_birth", ""))
+            binding.genderInputText.setText(getString("gender", ""))
+            binding.genderInputText.setText(getString("gender", ""))
+            binding.bloodGroupTextInput.setText(getString("blood_group", ""))
+            binding.genotypeTextInput.setText(getString("genotype", ""))
+            binding.countryTextInput.setText(getString("country", ""))
+            binding.stateTextInput.setText(getString("state", ""))
+            binding.cityTextInput.setText(getString("city", ""))
+        }
+    }
+
+    private fun handleViewsClick() {
+        binding.navigateUp.setOnClickListener {
+            dismiss()
+        }
+
+
+        binding.contactInfoButton.setOnClickListener {
+            ContactInfoDialogFragment().show(
+                parentFragmentManager,
+                getString(R.string.contact_info)
+            )
+        }
+
+        binding.stateTextInput.setOnClickListener {
+            StatesBottomSheetFragment { state ->
+                binding.stateTextInput.setText(state)
+            }.show(parentFragmentManager, getString(R.string.state))
+        }
+
+        binding.saveButton.setOnClickListener {
+            updateInfo()
+        }
+
+        binding.dateOfBirthTextInput.setOnClickListener {
+            DatePickerDialog(requireContext()) { date ->
+                binding.dateOfBirthTextInput.setText(date)
+            }.window?.setLayout(
+                LayoutParams.MATCH_PARENT,
+                LayoutParams.WRAP_CONTENT
+            )
+        }
+
+        binding.genderInputText.setOnClickListener {
+            GenderBottomSheetFragment { gender ->
+                binding.genderInputText.setText(gender)
+            }.show(parentFragmentManager, getString(R.string.gender))
+        }
+
+        binding.genotypeTextInput.setOnClickListener {
+            GenotypeBottomSheetFragment {
+                binding.genotypeTextInput.setText(it)
+            }.show(parentFragmentManager, getString(R.string.genotype))
+        }
+
+        binding.bloodGroupTextInput.setOnClickListener {
+            DonorBloodGroupsBottomSheetFragment{
+                binding.bloodGroupTextInput.setText(it)
+            }.show(parentFragmentManager, getString(R.string.blood_group))
+        }
+
+    }
+
+    private fun updateInfo() {
+        try {
+            val data = getDataFromForm()
+            if (isValidForm(data) && profileId != null) {
+                loadingDialog.show()
+                updatePersonalDetails(
+                    data = hashMapOf(
+                        "firstname" to data.firstname,
+                        "lastname" to data.lastname,
+                        "gender" to data.gender,
+                        "birthDate" to data.birthDate,
+                        "genotype" to data.genotype,
+                        "bloodGroup" to data.bloodGroup,
+                        "country" to data.country,
+                        "state" to data.state,
+                        "city" to data.city,
+                    ),
+                    userType = Constants.DONOR,
+                    profileId = data.id
+                ) { success, message ->
+                    if (success) {
+                        updateSharedPreferences(data)
+                        loadingDialog.dismiss()
+                        showToast("Saved successfully")
+                        dismiss()
+                    } else {
+                        loadingDialog.dismiss()
+                        showToast("Something went wrong please try again: $message")
+                    }
                 }
             }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            loadingDialog.dismiss()
+        }
     }
+
+    private fun updateSharedPreferences(donor: Donor) {
+        sharedPreferences.edit().run {
+            putString("firstname", donor.firstname)
+            putString("lastname", donor.lastname)
+            putString("gender", donor.gender)
+            putString("genotype", donor.genotype)
+            putString("date_of_birth", donor.genotype)
+            putString("country", donor.country)
+            putString("city", donor.city)
+            putString("state", donor.state)
+            putString("blood_group", donor.bloodGroup)
+            apply()
+        }
+    }
+
+    private fun isValidForm(donor: Donor): Boolean {
+        return when {
+            donor.firstname.isBlank() -> {
+                showToast("Please provide firstname")
+                false
+            }
+
+            donor.lastname.isBlank() -> {
+                showToast("Please provide lastname")
+                false
+            }
+
+            donor.birthDate.isBlank() -> {
+                showToast("Please provide date of birth")
+                false
+            }
+
+            donor.gender.isBlank() -> {
+                showToast("Please provide gender")
+                false
+            }
+
+            donor.genotype.isBlank() -> {
+                showToast("Please provide genotype")
+                false
+            }
+
+            donor.bloodGroup.isBlank() -> {
+                showToast("Please select blood group")
+                false
+            }
+
+            donor.country.isBlank() -> {
+                showToast("Please provide country")
+                false
+            }
+
+            donor.state.isBlank() -> {
+                showToast("Please provide state")
+                false
+            }
+
+            donor.city.isBlank() -> {
+                showToast("Please provide city")
+                false
+            }
+
+            else -> true
+        }
+    }
+
+    private fun getDataFromForm(): Donor {
+        return Donor(
+            id = "$profileId",
+            firstname = binding.firstNameTextInput.text.toString(),
+            lastname = binding.lastNameTextInput.text.toString(),
+            birthDate = binding.dateOfBirthTextInput.text.toString(),
+            bloodGroup = binding.bloodGroupTextInput.text.toString(),
+            gender = binding.genderInputText.text.toString(),
+            genotype = binding.genotypeTextInput.text.toString(),
+            country = binding.countryTextInput.text.toString().trim(),
+            state = binding.stateTextInput.text.toString(),
+            city = binding.cityTextInput.text.toString().trim()
+        )
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
+
 }
--- app/src/main/java/com/devtoochi/blood_donation/ui/fragments/DonorSignUpFragment.kt
+++ app/src/main/java/com/devtoochi/blood_donation/ui/fragments/DonorSignUpFragment.kt
@@ -1,41 +1,260 @@
 package com.devtoochi.blood_donation.ui.fragments
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.devtoochi.blood_donation.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.devtoochi.blood_donation.backend.firebase.AuthenticationManager
+import com.devtoochi.blood_donation.backend.firebase.PersonDetailsManager
+import com.devtoochi.blood_donation.backend.models.Donor
+import com.devtoochi.blood_donation.backend.models.User
+import com.devtoochi.blood_donation.backend.utils.Constants
+import com.devtoochi.blood_donation.backend.utils.Constants.DONOR
+import com.devtoochi.blood_donation.backend.utils.Util
+import com.devtoochi.blood_donation.databinding.FragmentDonorSignUpBinding
+import com.devtoochi.blood_donation.ui.activities.DonorDashboardActivity
+import com.devtoochi.blood_donation.ui.dialogs.LoadingDialog
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
 
 
 class DonorSignUpFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentDonorSignUpBinding
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var loadingDialog: LoadingDialog
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donor_sign_up, container, false)
+        binding = FragmentDonorSignUpBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        sharedPreferences =
+            requireActivity().getSharedPreferences(Constants.PREF_NAME, Context.MODE_PRIVATE)
+        loadingDialog = LoadingDialog(requireContext())
+
+        handleViewClick()
+    }
+
+    private fun handleViewClick() {
+        binding.signUpButton.setOnClickListener {
+            signUpWithEmailAndPassword()
+        }
+
+        binding.signInWithGoogle.setOnClickListener {
+            signUpWithGoogle()
+        }
+
+        binding.navigateBackButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        binding.passwordVisibilityToggle.setOnClickListener {
+            Util.togglePasswordVisibility(
+                binding.passwordInputText,
+                binding.passwordVisibilityToggle
+            )
+        }
+    }
+
+    private val googleSignInLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result?.data
+                val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+                handleGoogleSignInResult(task)
+            } else {
+                loadingDialog.dismiss()
+                showToast("Login failed. Please use another method to create account")
+            }
+        }
+
+    private fun signUpWithGoogle() {
+        loadingDialog.show()
+        val googleSignInClient = AuthenticationManager.googleSignInClient(requireContext())
+        val signInIntent = googleSignInClient.signInIntent
+        googleSignInLauncher.launch(signInIntent)
+    }
+
+    private fun handleGoogleSignInResult(task: Task<GoogleSignInAccount>) {
+        try {
+            val account = task.getResult(ApiException::class.java)
+
+            account?.let {
+                AuthenticationManager.registerWithGoogle(
+                    account = it,
+                    userType = DONOR
+                ) { success, message ->
+                    if (success) {
+                        if (message == DONOR) {
+                            PersonDetailsManager.getPersonalDetails(userType = DONOR) { user, exception ->
+                                handleAuthenticationResult(exception, user)
+                            }
+                        } else {
+                            loadingDialog.dismiss()
+                            AuthenticationManager.googleSignInClient(requireContext()).signOut()
+                            showToast("Email address already in use by another account")
+                        }
+                    } else {
+                        loadingDialog.dismiss()
+                        showToast(message.toString())
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            loadingDialog.dismiss()
+            showToast("An error occurred. ${e.message}")
+        }
+    }
+
+    private fun signUpWithEmailAndPassword() {
+        editTextWatcher(binding.emailTextInput, Constants.EMAIL)
+        editTextWatcher(binding.phoneNumberTextInput, Constants.PHONE_NUMBER)
+
+        try {
+            val user = getUserFromForm()
+
+            if (isValidSignUpForm(user)) {
+                loadingDialog.show()
+                AuthenticationManager.registerWithEmailAndPassword(user) { success, errorMessage ->
+                    if (success) {
+                        PersonDetailsManager.getPersonalDetails(userType = DONOR) { result, error ->
+                            handleAuthenticationResult(error, result)
+                        }
+                    } else {
+                        loadingDialog.dismiss()
+                        showToast(errorMessage.toString())
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            loadingDialog.dismiss()
+            showToast("An error occurred.")
+        }
+    }
+
+    private fun handleAuthenticationResult(errorMessage: String?, user: User?) {
+        when {
+            user != null -> {
+                Util.updateSharedPreferences(
+                    user = user,
+                    userType = DONOR,
+                    sharedPreferences = sharedPreferences
+                )
+                loadingDialog.dismiss()
+                navigateTo()
+            }
+
+            errorMessage == Constants.NOT_AVAILABLE -> showToastAndDismiss("User details not available")
+            else -> showToastAndDismiss(errorMessage.toString())
+        }
+    }
+
+    private fun navigateTo() {
+        startActivity(Intent(requireContext(), DonorDashboardActivity::class.java))
+        requireActivity().finish()
+    }
+
+    private fun showToastAndDismiss(message: String) {
+        loadingDialog.dismiss()
+        showToast(message)
+    }
+
+    private fun editTextWatcher(
+        completeTextView: MaterialAutoCompleteTextView,
+        from: String
+    ) {
+        completeTextView.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                val email = s.toString().trim()
+
+                if (Util.isValidEmailOrPhoneNumber(email, from)) {
+                    completeTextView.error = null
+                } else {
+                    completeTextView.error = if (from == Constants.EMAIL) {
+                        getString(R.string.invalid_email_address)
+                    } else {
+                        getString(R.string.invalid_phone_number)
+                    }
+                }
+            }
+        })
+    }
+
+    private fun getUserFromForm(): Donor {
+        return Donor(
+            email = binding.emailTextInput.text.toString().trim(),
+            password = binding.passwordInputText.text.toString().trim(),
+            firstname = binding.firstNameTextInput.text.toString().trim(),
+            lastname = binding.lastNameTextInput.text.toString().trim(),
+            phone = binding.phoneNumberTextInput.text.toString().trim(),
+        )
+    }
+
+    private fun isValidSignUpForm(donor: Donor): Boolean {
+        return when {
+            donor.firstname.isBlank() -> {
+                showToast("Please provide firstname")
+                false
+            }
+
+            donor.lastname.isBlank() -> {
+                showToast("Please provide lastname")
+                false
+            }
+
+            donor.phone.isBlank() -> {
+                showToast(getString(R.string.please_provide_phone_number))
+                false
+            }
+
+            donor.email.isBlank() -> {
+                showToast(getString(R.string.please_provide_email))
+                false
+            }
+
+            donor.password.isBlank() -> {
+                showToast(getString(R.string.please_provide_password))
+                false
+            }
+
+            else -> true
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
     }
 
 }
