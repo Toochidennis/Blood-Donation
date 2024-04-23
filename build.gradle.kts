9054f917ab9cb1bde1c2dcb166b3af9635baf04a
Toochidennis <dennistoochukwu@gmail.com>
2024-04-23 09:06:35 +0100

    Update

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+buildscript {
+    dependencies {
+        classpath(libs.androidx.navigation.safe.args)
+    }
+}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.googleServices) apply false
+}
